b118d89705a571a8ce4b15cf441b15ccaec99958
jboone2 <jboone2@uoregon.edu>
2020-04-22 16:55:55 -0700

    Add files for Project 2 Part 1.

--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,33 +1,30 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function(){
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+    let prefixURL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
     let suffixURL = "&format=json&jsoncallback=?";
     //get value entered by user from textbox
-    let flickrTag = $("input").???();
+    //let flickrTag = document.querySelector("input[type=text]").value;  <-- Old way.
+    let flickrTag = $("input").val(); // <-- three letters of the alphabet go here (first one is 'v') ('val'?)
     let requestURL = prefixURL + flickrTag + suffixURL;
 
     //clear old photos
-    $(".photos").???("");
+  //   document.querySelector(".photos").innerHTML = "";  //<-- Old way.
+    $(".photos").text("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
-
-      //Flickr returns 20 images by default
-      //We need only six images for the Gallery
+      //Flickr returns 20 images by default; we need only six images for the Gallery
       if (index < 6) {
         // create a new JQuery element to hold the image
         // but hide it so we can fade it in
         let $img = $("<img>").hide();
-
-        // set the attribute to the url
-        // contained in the response
+        // set the attribute to the url contained in the response
         $img.attr("src", item.media.m);
         $img.attr("width", "100");
 
-        // attach the img tag to the main
-        // photos element and then fade it in
+        // attach the img tag to the main photos element and then fade it in
         $(".photos").append($img);
         $img.fadeIn();
       }
@@ -35,6 +32,8 @@ let main = function(){
 
   });
 };
-
-
-$(document).ready(main);
+// Make the button work.
+window.addEventListener("load", () => {
+  let button = document.querySelector("button");
+  button.addEventListener("click", controller);
+});
